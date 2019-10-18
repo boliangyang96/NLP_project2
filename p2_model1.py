245ac70cf0e98012cb95eba6809b98e8dc8131d8
Boliang Yang <yangboliang1996@gmail.com>
2019-10-18 16:15:39 -0400

    add-k smoothing experiment

--- p2_model1.py
+++ p2_model1.py
@@ -36,6 +36,16 @@ def labelsCount(labelSeq):
             labels[i] += 1
     return labels
 
+# get word count
+def wordsCount(corpus):
+    words = {}
+    for line in corpus:
+        for word in line:
+            if word not in words:
+                words[word] = 0
+            words[word] += 1
+    return words
+
 # get transition probability between labels (i.e., P(t_i | t_(i-1)))
 # each key in this dictionary is in the format (t_(i-1), t_i))
 def transitionProbability(labelSeq, labelsCount):
@@ -58,17 +68,37 @@ def observationProbability(corpus, labelSeq, labelsCount):
             if (labelSeq[i][j], corpus[i][j]) not in observationProb:
                 observationProb[(labelSeq[i][j], corpus[i][j])] = 0
             observationProb[(labelSeq[i][j], corpus[i][j])] += 1
+    #min_ = 100
     for w in observationProb:
         observationProb[w] = 1.0 * observationProb[w] / labelsCount[w[0]]
+        #if (observationProb[w] < min_): min_ = observationProb[w]
+    #print(min_)
+    return observationProb
+
+# get observation probability with add-k smoothing
+# each key in this dictionary is in the format (t_i, w_i)
+def observationProbabilityWithAddK(corpus, labelSeq, labelsCount, wordsCount, k):
+    observationProb = {}
+    for i in range(len(corpus)):
+        for j in range(len(corpus[i])):
+            if (labelSeq[i][j], corpus[i][j]) not in observationProb:
+                observationProb[(labelSeq[i][j], corpus[i][j])] = 0
+            observationProb[(labelSeq[i][j], corpus[i][j])] += 1
+    for w in observationProb:
+        observationProb[w] = 1.0 * (observationProb[w] +  k) / (labelsCount[w[0]] + k * len(wordsCount))
     return observationProb
 
 # bigram viterbi
-def viterbi(corpus, transitionProb, observationProb, lambda_=1):
+def viterbi(corpus, transitionProb, observationProb, lambda_=1, addK=False, trainCorpus=[], trainLabelSeq=[], k=0, labelsCount={}, wordsCount={}):
     possibleLabels = [0, 1]
     output = []
+
+    if addK and k > 0:
+        observationProbWithAddK = observationProbabilityWithAddK(trainCorpus, trainLabelSeq, labelsCount, wordsCount, k)
+
     for line in corpus:
         n = len(line)
-        # dimension of the matrix is 3 x n
+        # dimension of the matrix is 2 x n
         score = np.zeros((2, n), dtype=np.float64)
         bptr = np.zeros((2, n), dtype=np.int8)
 
@@ -77,10 +107,24 @@ def viterbi(corpus, transitionProb, observationProb, lambda_=1):
         # initialization
         for i in range(2):
             transition = transitionProb[('<ls>', possibleLabels[i])]
-            try:
-                observation = observationProb[(possibleLabels[i], line[1])]
-            except KeyError:
-                observation = 1e-20
+            
+            # use add-k smoothing
+            if addK:
+                # if there is unknown word in test data, just assign a very very small value to the observation probability
+                if line[1] not in wordsCount:
+                    observation = 1e-20
+                # else if unseen bigram, use add-k (i.e., simply use the numerator as k)
+                elif (possibleLabels[i], line[1]) not in observationProbWithAddK:
+                    observation = 1.0 * k / (labelsCount[possibleLabels[i]] + k * len(wordsCount))
+                # else, get the observation probability
+                else: observation = observationProbWithAddK[(possibleLabels[i], line[1])]
+            # not use add-k smoothing
+            else:
+                try:
+                    observation = observationProb[(possibleLabels[i], line[1])]
+                except KeyError:
+                    observation = 1e-20
+
             score[i][1] = lambda_ * np.log(transition) + np.log(observation)
             bptr[i][1] = 0
 
@@ -99,10 +143,23 @@ def viterbi(corpus, transitionProb, observationProb, lambda_=1):
                 else:
                     maxScore = temp1
                     maxIndex = 1
-                try:
-                    observation = observationProb[(possibleLabels[i], line[t])]
-                except KeyError:
-                    observation = 1e-20
+
+                # use add-k smoothing
+                if addK:
+                    # if there is unknown word in test data, just assign a very very small value to the observation probability
+                    if line[t] not in wordsCount:
+                        observation = 1e-20
+                    # else if unseen bigram, use add-k (i.e., simply use the numerator as k)
+                    elif (possibleLabels[i], line[t]) not in observationProbWithAddK:
+                        observation = 1.0 * k / (labelsCount[possibleLabels[i]] + k * len(wordsCount))
+                    # else, get the observation probability
+                    else: observation = observationProbWithAddK[(possibleLabels[i], line[t])]
+                # not use add-k smoothing
+                else:
+                    try:
+                        observation = observationProb[(possibleLabels[i], line[t])]
+                    except KeyError:
+                        observation = 1e-20
                 score[i][t] = maxScore + np.log(observation)
                 bptr[i][t] = maxIndex
         
@@ -143,7 +200,23 @@ if __name__ == "__main__":
     observation = observationProbability(trainData[0], trainData[1], labelsCount)
     #print(observation)
     
-    # output the validation result
+    # validation with add-k smoothing experiment (with lambda = 0.5)
+    wordsCount = wordsCount(trainData[0])
+    # validationData = getCorpus("./data_release/val.csv")
+    # l = [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
+    # for i in l:
+    #     result = viterbi(validationData[0], transition, observation, 0.5, True, trainData[0], trainData[1], i, labelsCount, wordsCount)
+    #     outputFile = open('validation-add-%.1f.csv'%i, 'w')
+    #     outputFile.write('idx,label\n')
+    #     i = 1
+    #     for line in result:
+    #         for j in range(len(line)):
+    #             outputFile.write(str(i)+','+str(line[j])+'\n')
+    #             i += 1
+    #     outputFile.close()
+
+
+    # output the validation result, lambda experiment (no add-k here)
     # validationData = getCorpus("./data_release/val.csv")
     # l = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
     # for i in l:
@@ -157,10 +230,11 @@ if __name__ == "__main__":
     #             i += 1
     #     outputFile.close()
 
-    # output the test result
+
+    # output the test result (use lambda = 0.5, k = 0.1)
     # testData = getTestCorpus("./data_release/test_no_label.csv")
-    # result = viterbi(testData, transition, observation, 0.5)
-    # outputFile = open('test-result-lambda05.csv', 'w')
+    # result = viterbi(testData, transition, observation, 0.5, True, trainData[0], trainData[1], 0.1, labelsCount, wordsCount)
+    # outputFile = open('test-result-lambda05-add01.csv', 'w')
     # outputFile.write('idx,label\n')
     # i = 1
     # for line in result:
